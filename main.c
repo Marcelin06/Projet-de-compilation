e838242066d6d5ff1d334efa8b152fbe23b49150
Dieunel Marcelin <dieunel.marcelin@edu.univ-paris13.fr>
2024-01-30 11:55:01 +0100

    1er parseur

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
-#include <stdlib.h>
-#include <stdio.h>
+/* file main.c
+* compilation: gcc -o compiler main.c parser.tab.c lexer.tab.c
+* result: executable
+*/
 
-int main(void){
-    printf("\nHello World !\n");
+#include <stdio.h>
+#include <stdlib.h>
+#include "parser.tab.h"
+
+int main(void) {
+    if (!yyparse()) { // call to the parsing (and lexing) function
+        printf("\nParsing:: c'est bien une expression arithmétique\n"); // reached if parsing follows
+    }
     exit(EXIT_SUCCESS);
 }
